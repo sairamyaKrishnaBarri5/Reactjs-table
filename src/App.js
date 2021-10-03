4f463a0feae6dc02fa6003db5f8a46c093fa4d27
sairamyaBarri <79735526+sairamyaKrishnaBarri5@users.noreply.github.com>
2021-10-02 21:49:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,117 +1,173 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import SimpleReactValidator from 'simple-react-validator';
 import './table.css';
 
 
 export default function App() {
+  const [, forceUpdate] = useState();
+
+  const simpleValidator = useRef(new SimpleReactValidator());
   const [data, setData] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
   
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-  const handleChangedob = (event) => {
-    setSearchTerm(event.target.value);
-  };
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  let response;
+
   const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
+  const time = new Date().toLocaleTimeString();
 
+  
 
-  useEffect(() => {
+  const getData = (query) => {
     fetch("https://try.smilecdr.com/baseR4/Patient")
       .then((res) => res.json())
       .then((result) => {
         setData(result);
       });
-  }, []);
+  };
 
-  console.log(data.entry, "data");
+  const handleSearch = async () => {
+    const formValid = simpleValidator.current.allValid();
+    if (!formValid) {
+      simpleValidator.current.showMessages();
+      forceUpdate(1);
+      return;
+    }
+    
+
+  let params;
+  if(firstName && lastName) {
+    params = `?family=${firstName}&given=${lastName}`;
+  } else if(firstName && !lastName) {
+    params = `?family=${firstName}`;
+  } else if(!firstName && lastName) {
+    params = `?given=${lastName}`;
+  } else params = '';
+  response = await fetch(`https://try.smilecdr.com/baseR4/Patient${params}`)
+    .then((res) => res.json());
+  setData(response);
+};
+
   
+  
+  const handleReset = () => {
+    setFirstName("");
+    setLastName("");
+    getData();
+  };
+const [order,setorder] = useState("ASC");
+  const sorting =()=>{
+    if(order === "ASC"){
+      const sorted = [data].sort((a,b)=>
+      a.name.family.toLowerCase() > b.name.family.toLowerCase? 1 : -1
+      );
+      setData(sorted);
+      setorder("DSC");
+      
+    }
+    if(order === "DSC"){
+      const sorted = [data].sort((a,b)=>
+      a.toLowerCase() > b.toLowerCase()? 1 : -1
+      );
+      setData(sorted);
+      setorder("ASC");
+      
+    }
+  };
+
+  
+
+  useEffect(() => {
+    getData();
+  }, [response]);
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="App">
       <h2>SMILE CDR</h2>
-    <div className="search"> <input
-        type="text"
-        placeholder="Enter Name"
-        className="searchname"
-        value={searchTerm}
-       onChange={handleChange}/>
+      <div className="search">
+         
+        <input
+          type="text"
+          onkeyup="this.value = this.value.toLowerCase();"
+          placeholder="First Name"
+          className="searchname1"
+
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+        />
+        
        <input
-        type="text"
-        placeholder="dd/mm/yy"
-        className="searchname"
-        value={searchTerm}
-       onChange={handleChangedob}
-      
-
-      /><button className="reset">Reset</button></div>
-      
+          type="text"
+         
+          placeholder="Last Name"
+          className="searchname2"
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+        />
+        <button className="reset" onClick={handleSearch} style={{ marginRight: '10px' }}>Search</button>
+        <button className="reset" onClick={handleReset}>Reset</button>
+        <div className="firstname-error-message"> {simpleValidator.current.message('First Name', firstName, 'alpha_num_space')}</div>
+        <div className="lastname-error-message">{simpleValidator.current.message('Last Name', lastName, 'alpha_num_space')}</div>
+        
+      </div>
       <table>
-      <th>FirstName</th>
-      <th>LastName</th>
-      <th>Adress</th>
-      <th>phone</th>
-      <th>Gender</th>
-      <th>Birthday</th>
-        {data && (
+        <th onClick={()=>sorting("family")}>FistName</th>
+        <th>LastName</th>
+        <th>Adress</th>
+        <th>phone</th>
+        <th>Gender</th>
+        <th>Birthday</th>
+        {data && (data.entry) ? (
           <tbody>
-            {data.entry.filter((item, i)=>{
-              if(searchTerm ===""){
-                return item;
-              } else{
-                if(item.resource.name && item.resource.name[0].family.toLowerCase().includes(searchTerm.toLowerCase()) 
-                 ){
-                  return item;
-                }
+            {(data.entry)
+              .map((item, i) => (
+                <tr key={i}>
+                  <td>
+                    {" "}
+                    {(item.resource.name && item.resource.name[0].family) || "-"}
+                  </td>
+                  <td>
+                    {" "}
+                    {(item.resource.name && item.resource.name[0].given) || "-"}
+                  </td>
 
-              }
 
-              
-            })
-            .map((item, i) => (
-              <tr>
-               <td>
-                   {" "}
-                  {(item.resource.name && item.resource.name[0].family) || "-"}
-                </td>
-                <td>
-                  {" "}
-                  {(item.resource.name && item.resource.name[0].given) || "-"}
-                </td>
-                
-                
-                <td>
-                  {" "}
-                  {(item.resource.address && item.resource.address[0].line) || "-"}<br/>
-                  {(item.resource.address && item.resource.address[0].state) || "-"}<br/>
-                  {(item.resource.address && item.resource.address[0].postalCode) || "-"}
-                </td>
-                <td>
-                  {" "}
-                  {(item.resource.telecom && item.resource.telecom[0].value) || "-"}
-                </td>
-                
+                  <td>
+                    {" "}
+                    {(item.resource.address && item.resource.address[0].line) || "-"}<br />
+                    {(item.resource.address && item.resource.address[0].state) || "-"}<br />
+                    {(item.resource.address && item.resource.address[0].postalCode) || "-"}
+                  </td>
+                  <td>
+                    {" "}
+                    {(item.resource.telecom && item.resource.telecom[0].value) || "-"}
+                  </td>
+                  <td>
+                    {" "}
+                    {(item.resource.gender) || "-"}
+                  </td>
+                  <td>
+                    {" "}
+                    {(item.resource.birthDate) || "-"}
+                  </td>
 
-                
-                <td>
-                  {" "}
-                  {(item.resource.gender) || "-"}
-                </td>
-                <td>
-                  {" "}
-                  {(item.resource.birthDate) || "-"}
-                </td>
-                
-              </tr>
-            ))}
+                </tr>
+              ))}
           </tbody>
+        ) : (
+            <tr style={{ textAlign: 'center' }}>
+              No results found for this search.
+            </tr>
         )}
       </table>
-  <div className="datetime container">{date} {time}</div>
+      <div className="datetime container">{date} {time}</div>
 
     </div>
-  
+
 
 
 
@@ -139,6 +195,8 @@ const time = new Date().toLocaleTimeString();
 
 
   );
-  
+
 }
+
+
 
